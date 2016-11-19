b75b381425cf6e9a785d70e64fca72054b5225d8
nkpmedia <webmaster@nkp-media.de>
2016-11-19 23:58:05 +0100

    Added first unit Test (index redirect)

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -6,13 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testIndexRedirect()
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
     }
 }
