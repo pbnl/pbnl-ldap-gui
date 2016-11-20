71b7c44d228da2b4e0716e7b2171465e291987b3
nkpmedia <webmaster@nkp-media.de>
2016-11-20 20:04:33 +0100

    Added a Login function test

---
+++ tests/AppBundle/Controller/LoginControllerTest.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: paul
+ * Date: 20.11.16
+ * Time: 18:55
+ */
+
+namespace Tests\AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+
+class LoginControllerTest extends WebTestCase
+{
+    public function testLogout()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/logout');
+
+        $crawler = $client->request('GET', '/logout');
+
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+    }
+
+    public function testLogin()
+    {
+        //Correct login
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/logout');
+
+        $crawler = $client->request('GET', '/login');
+
+        $form = $crawler->selectButton('Login')->form();
+
+        $form['form[name]'] = 'Paul';
+        $form['form[password]'] = 'passwd';
+
+        $crawler = $client->submit($form);
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+
+        //Uncorrect login
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/logout');
+
+        $crawler = $client->request('GET', '/login');
+
+        $form = $crawler->selectButton('Login')->form();
+
+        $form['form[name]'] = 'Paul';
+        $form['form[password]'] = 'hans';
+
+        $crawler = $client->submit($form);
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("Name oder Passwort falsch!")')->count()
+        );
+
+    }
+
+    public function testPermissionsError()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/startPage');
+        var_dump("efef");
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("permissionError")')->count()
+        );
+    }
+}
