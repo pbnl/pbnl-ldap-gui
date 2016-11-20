b3167c63287df272823922d6b63e175dd9ff437f
nkpmedia <webmaster@nkp-media.de>
2016-11-20 20:38:16 +0100

    Changed file name so that the tester will test it

--- tests/AppBundle/Controller/ShowAllUserTest.php
+++ tests/AppBundle/Controller/ShowAllUserTest.php
@@ -13,7 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Tests\AppBundle\LoginAction;
 
 
-class ShowAllUserTestClass extends WebTestCase
+class ShowAllUserTest extends WebTestCase
 {
     public function testShowAllUsers()
     {
